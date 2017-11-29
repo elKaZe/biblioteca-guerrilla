4dbc009881346b6362dbc97e02664100a2189f4e
KaZe <kaze@partidopirata.com.ar>
2017-11-28 23:46:00 -0300

    heleper para tests del conector de calibre

--- app/conector/calibre/tests/calibre_tests_helper.py
+++ app/conector/calibre/tests/calibre_tests_helper.py
@@ -2,16 +2,47 @@
 # -*- coding: utf-8 -*-
 
 
-import factory
+import faker
+import tempfile
+import subprocess
 
 class CalibreTestsHelper(object):
 
     """Get rid of books and relations"""
 
-    def __init__(self, library_path="/tmp/"):
+    def __init__(self, library_directory="/tmp/"):
         """
 
-        :library_path: path to create the library
+        :library_directory: directroy that will contain the library directory
 
         """
-        self._library_path = library_path
+        self._library_directory= library_directory
+        self.library_path = tempfile.mkdtemp(dir=self._library_directory)
+
+        # calibredb and specifies the library path
+        self.base_command = ["calibredb", "--library-path",self.library_path]
+
+    def _run_command(self, command):
+        """Run specified command
+
+        :command: iterable object
+        :returns: subprocess.run object
+
+        """
+        return subprocess.run(command, shell=True, check=True)
+
+
+    def create_empty_library(self):
+        """Create an empty library
+        """
+        command =  self.base_command
+
+        _run_command(command)
+
+
+
+    def destroy(self):
+        """Will delete temporal library
+        """
+        self.library_path.close()
+
