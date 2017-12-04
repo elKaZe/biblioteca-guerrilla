498757a80a3266f37d7ac26c5cf97bf6ceb405ea
KaZe <kaze@partidopirata.com.ar>
2017-12-03 21:44:31 -0300

    Agregado directorio de destino para la web estatica

--- app/settings.py
+++ app/settings.py
@@ -4,11 +4,16 @@
 #
 # Distributed under terms of the GPLv3+ license.
 
+######### Connector
 CONECTOR = "conector.calibre.calibre"
 CONECTOR_OPCIONES = {
     "ruta": "tests/data/biblioteca_calibre2/metadata.db",
 }
 RUTA_BASE_LIBROS = "tests/data/biblioteca_calibre2"
 
+######### Frozen-Flask
+FREEZER_DESTINATION = "/tmp/biblioteca-guerrilla/"
+
+######### Languages
 SUPPORTED_LANGUAGES = {'es': 'Español', 'en': 'English'}
 BABEL_DEFAULT_LOCALE = "es"
