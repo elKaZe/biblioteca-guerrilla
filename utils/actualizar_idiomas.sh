13fb5545bbf6324f973b7b8afb56ea3246783c48
KaZe <kaze@partidopirata.com.ar>
2018-02-02 15:03:59 -0300

    movido todo a una nueva carpeta

--- utils/actualizar_idiomas.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/env bash
-
-# Actualizar po de idiomas
-
-pybabel update -i messages.pot -d app/translations
--- utils/compilar_idiomas.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/env bash
-
-# Compila los idiomas
-
-pybabel compile -f -d app/translations
